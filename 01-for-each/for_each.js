59f5d5b627596333d2fab9b6428f3c3e65a69b2c
lyonelle <ambouloulyo@gmail.com>
2021-06-22 17:26:48 +0200

    esercizio # modificato

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -1,7 +1,7 @@
 // define an array of fruits
 const fruitsSaison = ['Cache', 'orange', 'pear', 'cherry', 'grape']
 console.log(fruitsSaison)
-const printFruits = (fruit, index) => console.log(`${index + 1} ${fruit}`) 
+const printFruits = (fruit, index) => console.log(`${index + 1} ) ${fruit}`) 
     // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
   fruitsSaison.forEach(printFruits)
  //const fruitsSaison(fruits, index) => console.log(`${index + 1} ${fruit}`) 
