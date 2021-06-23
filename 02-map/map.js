a8859f52058b891899e687dced84925ec12ae601
lyonelle <ambouloulyo@gmail.com>
2021-06-23 10:51:14 +0200

    esercizio # completato

--- 02-map/map.js
+++ 02-map/map.js
@@ -1,13 +1,14 @@
 const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-const secretMessage = // use .map to get the first letter of every animal name in the arrray animals
+const secretMessage = animals.map(animal => animal[0]);
+  // use .map to get the first letter of every animal name in the arrray animals
 
-console.log(secretMessage.join(''));
+ console.log(secretMessage.join(''));
 
 
-const bigNumbers = [100, 200, 300, 400, 500];
+ const bigNumbers = [100, 200, 300, 400, 500];
 
-// Create the smallNumbers array below with map
-
+//  Create the smallNumbers array below with map
+const smallNumbers = bigNumbers.map(bigNumber => bigNumber / 10);
 
 console.log(smallNumbers);
